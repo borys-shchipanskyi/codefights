b1c650e6091b89323c7a2cb4065815765bbf2ed8
Shchypanskyi, Borys <boris.shchipanskiy@gmail.com>
2017-11-30 16:57:15 -0500

    Added hash table solutions

--- interviewPractice/solutions/hashTables.go
+++ interviewPractice/solutions/hashTables.go
@@ -42,11 +42,64 @@ func (table *HashTable) GetKeys()[]string{
 func (table *HashTable) GetValues(key string)[]string{
 	keyIndex := table.getKeyIndex(key)
 	res := []string{}
+	if keyIndex == -1{
+		return res
+	}
 	for _, value := range table.values[keyIndex]{
 		res = append(res, value)
 	}
 
-	return table.values[keyIndex]
+	return res
+}
+
+type HashTableInt struct {
+	keys []int
+	values [][]int
+}
+
+func (table *HashTableInt) getKeyIndex(key int )int{
+	for i, value := range table.keys{
+		if value == key{
+			return i
+		}
+	}
+	return -1
+}
+
+func  (table *HashTableInt) addKey(key int) int{
+	table.keys = append(table.keys, key)
+	table.values = append(table.values, []int{})
+	return len(table.keys) -1
+}
+
+func (table *HashTableInt) Add(key int, value int){
+	keyIndex := table.getKeyIndex(key)
+	if keyIndex == -1{
+		keyIndex = table.addKey(key)
+	}
+	table.values[keyIndex] = append(table.values[keyIndex], value)
+}
+
+func (table *HashTableInt) GetKeys()[]int{
+	res := []int{}
+	for _, key := range table.keys{
+		res = append(res, key)
+	}
+
+	return res
+}
+
+func (table *HashTableInt) GetValues(key int)[]int{
+	keyIndex := table.getKeyIndex(key)
+	res := []int{}
+	if keyIndex == -1{
+		return res
+	}
+	for _, value := range table.values[keyIndex]{
+		res = append(res, value)
+	}
+
+	return res
 }
 
 
@@ -117,6 +170,180 @@ func GroupingDishes(dishes [][]string) [][]string {
 
 			}
 			res = append(res, row)
+		}
+	}
+	return res
+}
+
+func AreFollowingPatterns(strings []string, patterns []string) bool {
+	hashTable := HashTable{}
+	for i:= 0; i < len(strings); i++{
+		key := patterns[i]
+		value := strings[i]
+		values := hashTable.GetValues(key)
+		if len(values) == 0{
+			hashTable.Add(key, value)
+		} else {
+			if values[0] != value{
+				return false
+			}
+		}
+
+	}
+	hashTable = HashTable{}
+	for i:= 0; i < len(strings); i++{
+		value := patterns[i]
+		key := strings[i]
+		values := hashTable.GetValues(key)
+		if len(values) == 0{
+			hashTable.Add(key, value)
+		} else {
+			if values[0] != value{
+				return false
+			}
+		}
+
+	}
+
+	return true
+}
+
+func ContainsCloseNums(nums []int, k int) bool {
+	hashTable := HashTableInt{}
+
+	for i, n := range nums{
+		hashTable.Add(n, i)
+	}
+	fmt.Printf("%#v\n", hashTable)
+	for _, key := range hashTable.GetKeys(){
+		values := hashTable.GetValues(key)
+		if len(values) <2 {
+			continue
+		}
+		for j := 1; j< len(values); j++{
+			if (values[j] - values[j-1]) <= k{
+				return true
+			}
+
+		}
+	}
+	return false
+}
+
+func PossibleSums(coins []int, quantity []int) int {
+	out := map[int]int{}
+	res := []int {}
+	pos := 0
+	for i, val := range coins{
+		for j := 1; j <= quantity[i]; j++{
+			res = append(res, j*val)
+			out[j*val] = -1
+			if i > 0 {
+				for k := 0; k < pos; k++{
+					res = append(res, j*val + res[k])
+					out[j*val + res[k]] = -1
+				}
+			}
+		}
+		pos = len(res)
+	}
+	return len(out)
+}
+
+
+// SwapLexOrder
+
+func get_pair_for_index(pairs *[][]int, index int)[]int{
+	res := []int{}
+	for _, pair := range *pairs{
+		if pair[0] == index{
+			res = append(res, pair[1])
+		}
+		if pair[1] == index{
+			res = append(res, pair[0])
+		}
+	}
+	return res
+}
+func isContains( target *[]int, val int) bool{
+	for _, el := range *target{
+		if el == val{
+			return true
+		}
+	}
+	return false
+}
+
+func marge_two_slices(slice1 *[]int, slice2 *[]int){
+	for _, el := range *slice2{
+		if !isContains(slice1, el){
+			*slice1 = append(*slice1, el)
+		}
+	}
+}
+
+func find_pairs_for_index(pairs *[][]int, index int)[]int{
+	res := []int{index}
+	res = append(res, get_pair_for_index(pairs, index)...)
+	for i:=1; i<len(res); i++{
+		tmp := get_pair_for_index(pairs, res[i])
+		marge_two_slices(&res, &tmp)
+	}
+	return res
+}
+
+func generate_indexes_map(pairs *[][]int)map[int][]int{
+	res := map[int][]int{}
+
+	for _, pair := range *pairs{
+		if _, ok := res[pair[0]]; !ok{
+			res[pair[0]] = find_pairs_for_index(pairs, pair[0])
+		}
+		if _, ok := res[pair[1]]; !ok{
+			res[pair[1]] = find_pairs_for_index(pairs, pair[1])
+		}
+	}
+	return res
+}
+
+func get_right_letter(usedIndexes *[]int, indexes *[]int, str string)string{
+	free_indexes := []int{}
+	for _, in := range *indexes{
+		if (!isContains(usedIndexes, in)){
+			free_indexes = append(free_indexes, in)
+		}
+	}
+	max := str[free_indexes[0]-1]
+	usedIndex := free_indexes[0]
+	for i:=1; i < len(free_indexes); i++{
+		if str[free_indexes[i] - 1] > max{
+			max = str[free_indexes[i]-1]
+			usedIndex = free_indexes[i]
+		}
+	}
+	*usedIndexes = append(*usedIndexes, usedIndex)
+	return string(max)
+}
+
+func SwapLexOrder(str string, pairs [][]int) string {
+	if len(pairs) < 2{
+		return str
+	}
+	res := ""
+	usedIndexes := []int{}
+	indexes_map := generate_indexes_map(&pairs)
+
+	for i:= 1; i <= len(str); i++{
+		if val, ok := indexes_map[i]; ok{
+			if len(val) == 1{
+				res += string(str[val[0] -1])
+			} else {
+				res += get_right_letter(&usedIndexes, &val, str)
+			}
+
+		} else {
+			res += string(str[i -1])
+
 		}
 	}
 	return res
--- interviewPractice/tests/hashTables_test.go
+++ interviewPractice/tests/hashTables_test.go
@@ -7,12 +7,10 @@ import (
 	"fmt"
 )
 
-
-
 func TestGroupingDishes(t *testing.T) {
 	tests := []struct {
-		input  [][]string
-		out [][]string
+		input [][]string
+		out   [][]string
 	}{
 		{
 			[][]string{{"Salad", "Tomato", "Cucumber", "Salad", "Sauce"},
@@ -27,12 +25,129 @@ func TestGroupingDishes(t *testing.T) {
 	}
 	for _, test := range tests {
 		res := GroupingDishes(test.input)
-		fmt.Println("************************")
 		PrintHashTable(test.input)
 		fmt.Println("Got res: ")
 		PrintHashTable(res)
 		if !IsHashTablesEquals(test.out, res) {
 			t.Errorf("Test input: %#v, Expect: %#v, but get: %#v\n", test.input, test.out, res)
+		}
+	}
+
+}
+
+func TestAreFollowingPatterns(t *testing.T) {
+	tests := []struct {
+		strings  []string
+		patterns []string
+		out      bool
+	}{
+		{
+			[]string{"cat", "dog", "dog"},
+			[]string{"a", "b", "b"},
+			true,
+		},
+		{
+			[]string{"cat", "dog", "dog"},
+			[]string{"a", "b", "c"},
+			false,
+		},
+		{
+			[]string{"cat", "dog", "doggy"},
+			[]string{"a", "b", "b"},
+			false,
+		},
+
+	}
+
+	for _, test := range tests {
+		if AreFollowingPatterns(test.strings, test.patterns) != test.out {
+			t.Error("Test input error")
+			fmt.Printf("%#v\n", test)
+		}
+	}
+}
+
+func TestContainsCloseNums(t *testing.T) {
+	tests := []struct {
+		coins    []int
+		quantity []int
+		out      int
+	}{
+		{
+			[]int{10, 50, 100},
+			[]int{1, 2, 1},
+			9,
+		},
+		{
+			[]int{10, 50, 100, 500},
+			[]int{5, 3, 2, 2},
+			122,
+		},
+		{
+			[]int{1, 2},
+			[]int{50000, 2},
+			50004,
+		},
+	}
+
+	for _, test := range tests {
+		res := PossibleSums(test.coins, test.quantity)
+		if res != test.out {
+			t.Error("Test input error")
+			fmt.Printf("%#v\n", res)
+		}
+	}
+}
+
+func TestPossibleSums(t *testing.T) {
+	tests := []struct {
+		coins    []int
+		quantity []int
+		out      int
+	}{
+		{
+			[]int{10, 50, 100},
+			[]int{1, 2, 1},
+			9,
+		},
+		{
+			[]int{10, 50, 100, 500},
+			[]int{5, 3, 2, 2},
+			122,
+		},
+	}
+
+	for _, test := range tests {
+		res := PossibleSums(test.coins, test.quantity)
+		if res != test.out {
+			t.Error("Test input error")
+		}
+	}
+
+}
+
+func TestSwapLexOrder(t *testing.T) {
+	tests := []struct {
+		str   string
+		pairs [][]int
+		out   string
+	}{
+		{
+			"abdc",
+			[][]int{{1, 4}, {3, 4}},
+			"dbca",
+		},
+		{
+			"acxrabdz",
+			[][]int{{1, 3}, {6, 8}, {3, 8}, {2, 7}},
+			"zdxrabca",
+		},
+	}
+
+	for _, test := range tests {
+		res := SwapLexOrder(test.str, test.pairs)
+		if res != test.out {
+			t.Error("Test input error")
 		}
 	}
 
